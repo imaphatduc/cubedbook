5dd89ffad101a2793bc59490cce08557ec03a79b
imaphatduc <vutranducphat@gmail.com>
2022-07-13 21:02:54 +0700

    Value prop for `InputField`

--- components/fields/InputField.tsx
+++ components/fields/InputField.tsx
@@ -2,14 +2,15 @@ import { ChangeEvent, FC } from 'react';
 
 interface Props {
   label: string;
+  value?: string | number;
   handler: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const InputField: FC<Props> = ({ label, handler }) => {
+const InputField: FC<Props> = ({ label, value, handler }) => {
   return (
     <div className="d-flex justify-content-between my-3">
       <p>{label}</p>
-      <input onChange={handler} style={{ maxWidth: 100 }} />
+      <input value={value} onChange={handler} style={{ maxWidth: 100 }} />
     </div>
   );
 };
