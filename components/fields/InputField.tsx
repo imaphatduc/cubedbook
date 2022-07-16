80aa3f0d3affe6228b6ee1f057b6fce21ccacec2
imaphatduc <vutranducphat@gmail.com>
2022-07-16 20:00:05 +0700

    Add types for `InputField` values

--- components/fields/InputField.tsx
+++ components/fields/InputField.tsx
@@ -1,16 +1,22 @@
-import { ChangeEvent, FC } from 'react';
+import { ChangeEvent, FC, HTMLInputTypeAttribute } from 'react';
 
 interface Props {
   label: string;
+  type?: HTMLInputTypeAttribute;
   value?: string | number;
   handler: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const InputField: FC<Props> = ({ label, value, handler }) => {
+const InputField: FC<Props> = ({ label, type, value, handler }) => {
   return (
     <div className="d-flex justify-content-between my-3">
       <p>{label}</p>
-      <input value={value} onChange={handler} style={{ maxWidth: 100 }} />
+      <input
+        type={type}
+        value={value}
+        onChange={handler}
+        style={{ maxWidth: 100 }}
+      />
     </div>
   );
 };
