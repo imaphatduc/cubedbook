a8d432d390aa653b70f8a508f0b5c47ac493d290
imaphatduc <vutranducphat@gmail.com>
2022-07-18 11:02:08 +0700

    Default duration for `Create` animation and its input field value + type

--- components/scratchpads/CreatePad.tsx
+++ components/scratchpads/CreatePad.tsx
@@ -6,6 +6,9 @@ import Scratchpad from '../Scratchpad';
 import CreateEditPad from './CreateEditPad';
 import CreatePlayPad from './CreatePlayPad';
 
+import { useNodes } from '../../contexts/NodesContext';
+import { useCubed } from '../../contexts/CubedContext';
+
 interface Props {
   nodeId?: string;
 
@@ -13,8 +16,16 @@ interface Props {
 }
 
 const CreatePad: FC<Props> = ({ nodeId, setCurrentPad }) => {
+  const { cubed } = useCubed();
+  const { nodes } = useNodes();
+
+  const currentNode = nodes.filter((node) => node.id === nodeId)[0];
+
   const [cubiconNodeId, setCubiconNodeId] = useState('');
-  const [duration, setDuration] = useState(0);
+
+  const [duration, setDuration] = useState(
+    currentNode?.object.duration ?? cubed!.ANIME.CREATE
+  );
 
   const handleDurationInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setDuration(parseInt(e.target.value));
@@ -37,7 +48,13 @@ const CreatePad: FC<Props> = ({ nodeId, setCurrentPad }) => {
       )}
 
       <DropField label="cubicon" setCubiconNodeId={setCubiconNodeId} />
-      <InputField label="duration" handler={handleDurationInputChange} />
+
+      <InputField
+        label="duration"
+        type={'number'}
+        value={duration}
+        handler={handleDurationInputChange}
+      />
     </Scratchpad>
   );
 };
