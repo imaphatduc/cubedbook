668b49051909a699d71c30be64f7eda1ba7c3f67
imaphatduc <vutranducphat@gmail.com>
2022-12-01 14:24:33 +0700

    Context menu in app frontpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import { NodesSection } from './components/sections/NodesSection';
+import { MouseEvent, useState } from 'react';
+import { useMenuState } from '@szhsin/react-menu';
+
 import { PadSection } from './components/sections/PadSection';
 import { SceneSection } from './components/sections/SceneSection';
+import { CtxMenu } from './components/menu/CtxMenu';
 
 function App() {
+  const [menuProps, toggleMenu] = useMenuState();
+  const [anchorPoint, setAnchorPoint] = useState({ x: 0, y: 0 });
+
+  const openContextMenu = (e: MouseEvent) => {
+    e.preventDefault();
+    setAnchorPoint({ x: e.clientX, y: e.clientY });
+    toggleMenu(true);
+  };
+
   return (
     <div className="App">
-      <div className="grid grid-cols-[1fr_2fr_1fr] bg-[#111]">
-        <div className="prose grid grid-rows-[1fr_1fr] my-2 px-5">
+      <div
+        className="grid grid-cols-[1fr_2fr_1fr] bg-[#222] text-white"
+        onContextMenu={openContextMenu}
+      >
+        <div className="prose my-2 px-5">
           <SceneSection />
-          <NodesSection />
         </div>
         <div
           id="cubecubed"
@@ -17,6 +31,12 @@ function App() {
         <div className="prose my-2 px-5">
           <PadSection />
         </div>
+
+        <CtxMenu
+          menuProps={menuProps}
+          toggleMenu={toggleMenu}
+          anchorPoint={anchorPoint}
+        />
       </div>
     </div>
   );
