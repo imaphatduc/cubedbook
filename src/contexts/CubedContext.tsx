0a6ef96c06b1d7a0b455680bbd363c5ac0f13477
imaphatduc <vutranducphat@gmail.com>
2022-12-01 18:22:17 +0700

    CubedContext provider

---
+++ src/contexts/CubedContext.tsx
@@ -0,0 +1,56 @@
+import {
+  createContext,
+  FC,
+  PropsWithChildren,
+  useContext,
+  useState,
+} from 'react';
+
+import { Cubicon, Group, Scene } from 'cubecubed';
+
+export interface ICubiconNode {
+  name: string;
+  cubicon: Cubicon;
+}
+
+export interface IGroupNode {
+  type?: '2d';
+  name: string;
+  group: Group;
+  cubiconNodes: ICubiconNode[];
+}
+
+interface ContextValue {
+  scene: Scene;
+  groupNodes: IGroupNode[];
+  addGroup: (name: string, type: '2d' | '3d') => void;
+}
+
+const CubedContext = createContext<ContextValue>(null);
+
+export const useCubed = () => useContext(CubedContext);
+
+export const CubedProvider: FC<PropsWithChildren> = ({ children }) => {
+  const scene = new Scene('simpleScene');
+
+  const [groupNodes, setGroupNodes] = useState<IGroupNode[]>([]);
+
+  const addGroup = (name: string, type: '2d' | '3d') => {
+    if (type === '2d') {
+      const groupNode: IGroupNode = {
+        type,
+        name,
+        group: new Group(name, scene),
+        cubiconNodes: [],
+      };
+
+      setGroupNodes([...groupNodes, groupNode]);
+    }
+  };
+
+  return (
+    <CubedContext.Provider value={{ scene, groupNodes, addGroup }}>
+      {children}
+    </CubedContext.Provider>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { CubedProvider } from './contexts/CubedContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
+  <CubedProvider>
+    <App />
+  </CubedProvider>
 );
