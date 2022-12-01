0c3828c0fe1bc3ce203e3048d1a6ee18ba9761eb
imaphatduc <vutranducphat@gmail.com>
2022-12-01 18:13:41 +0700

    Change `SectionHeader` styles

--- src/components/sections/components/SectionHeader.tsx
+++ src/components/sections/components/SectionHeader.tsx
@@ -3,7 +3,7 @@ interface Props {
 }
 
 const SectionHeader = ({ header }: Props) => {
-  return <h4 className="text-blue-400 my-3 font-medium">{header}</h4>;
+  return <h3 className="text-blue-300 my-3 font-medium">{header}</h3>;
 };
 
 export default SectionHeader;
