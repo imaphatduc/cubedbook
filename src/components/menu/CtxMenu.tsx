ebd80945fb8bbc4ce7e0e5f1b2e43a32656a216c
imaphatduc <vutranducphat@gmail.com>
2022-12-01 19:06:57 +0700

    Add Tailwind class to `CtxMenu` component

--- src/components/menu/CtxMenu.tsx
+++ src/components/menu/CtxMenu.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from 'react';
 
 export const menuClassName = ({ state }: { state: MenuState }) =>
   `box-border z-50 text-sm bg-[#444] rounded-sm shadow-xl select-none focus:outline-none min-w-[12rem] ${
-    state === 'open' && 'animate-fadeIn'
+    state === 'open' && 'animate-fadeIn space-y-2 p-0 pb-2 list-none'
   } ${state === 'closed' && 'animate-fadeOut'}`;
 
 interface Props {
