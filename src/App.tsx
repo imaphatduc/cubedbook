455c62258f032c62450fea59d21435ac2383211e
imaphatduc <vutranducphat@gmail.com>
2022-12-01 18:14:10 +0700

    Generalize the use of `CtxMenu`

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { useMenuState } from '@szhsin/react-menu';
 import { PadSection } from './components/sections/PadSection';
 import { SceneSection } from './components/sections/SceneSection';
 import { CtxMenu } from './components/menu/CtxMenu';
+import { CtxSubMenu } from './components/menu/CtxSubMenu';
+import { CtxMenuItem } from './components/menu/CtxMenuItem';
 
 function App() {
   const [menuProps, toggleMenu] = useMenuState();
@@ -36,7 +38,35 @@ function App() {
           menuProps={menuProps}
           toggleMenu={toggleMenu}
           anchorPoint={anchorPoint}
-        />
+        >
+          <CtxSubMenu label="Geometry">
+            <CtxMenuItem label="Rectangle" />
+            <CtxMenuItem label="Square" />
+            <CtxMenuItem label="Circle" />
+            <CtxMenuItem label="Line" />
+            <CtxMenuItem label="Vector Shape" />
+            <CtxMenuItem label="Parametric Curve" />
+          </CtxSubMenu>
+          <CtxSubMenu label="Coordinates System">
+            <CtxMenuItem label="Axes" />
+          </CtxSubMenu>
+          <CtxSubMenu label="Animations">
+            <CtxMenuItem label="Create Shape" />
+            <CtxMenuItem label="Create Vector Shape" />
+            <CtxMenuItem label="Translate" />
+            <CtxMenuItem label="Rotate" />
+            <CtxMenuItem label="Fade In" />
+            <CtxMenuItem label="Fade Out" />
+            <CtxMenuItem label="Draw Grid" />
+            <CtxMenuItem label="Draw Axes" />
+            <CtxMenuItem label="Draw Vector Field" />
+            <CtxMenuItem label="Point To Coordinates" />
+            <CtxMenuItem label="Point Along Graph" />
+            <CtxMenuItem label="Write" />
+            <CtxMenuItem label="Trace" />
+            <CtxMenuItem label="Apply Function" />
+          </CtxSubMenu>
+        </CtxMenu>
       </div>
     </div>
   );
--- src/components/menu/CtxMenu.tsx
+++ src/components/menu/CtxMenu.tsx
@@ -1,6 +1,5 @@
-import { ControlledMenu, MenuState, useMenuState } from '@szhsin/react-menu';
-import { CtxMenuItem } from './CtxMenuItem';
-import { CtxSubMenu } from './CtxSubMenu';
+import { ControlledMenu, MenuState } from '@szhsin/react-menu';
+import { PropsWithChildren } from 'react';
 
 export const menuClassName = ({ state }: { state: MenuState }) =>
   `box-border z-50 text-sm bg-[#444] rounded-sm shadow-xl select-none focus:outline-none min-w-[12rem] ${
@@ -19,7 +18,12 @@ interface Props {
   };
 }
 
-export const CtxMenu = ({ menuProps, toggleMenu, anchorPoint }: Props) => {
+export const CtxMenu = ({
+  children,
+  menuProps,
+  toggleMenu,
+  anchorPoint,
+}: PropsWithChildren<Props>) => {
   return (
     <ControlledMenu
       {...menuProps}
@@ -27,33 +31,7 @@ export const CtxMenu = ({ menuProps, toggleMenu, anchorPoint }: Props) => {
       anchorPoint={anchorPoint}
       onClose={() => toggleMenu(false)}
     >
-      <CtxSubMenu label="Geometry">
-        <CtxMenuItem label="Rectangle" />
-        <CtxMenuItem label="Square" />
-        <CtxMenuItem label="Circle" />
-        <CtxMenuItem label="Line" />
-        <CtxMenuItem label="Vector Shape" />
-        <CtxMenuItem label="Parametric Curve" />
-      </CtxSubMenu>
-      <CtxSubMenu label="Coordinates System">
-        <CtxMenuItem label="Axes" />
-      </CtxSubMenu>
-      <CtxSubMenu label="Animations">
-        <CtxMenuItem label="Create Shape" />
-        <CtxMenuItem label="Create Vector Shape" />
-        <CtxMenuItem label="Translate" />
-        <CtxMenuItem label="Rotate" />
-        <CtxMenuItem label="Fade In" />
-        <CtxMenuItem label="Fade Out" />
-        <CtxMenuItem label="Draw Grid" />
-        <CtxMenuItem label="Draw Axes" />
-        <CtxMenuItem label="Draw Vector Field" />
-        <CtxMenuItem label="Point To Coordinates" />
-        <CtxMenuItem label="Point Along Graph" />
-        <CtxMenuItem label="Write" />
-        <CtxMenuItem label="Trace" />
-        <CtxMenuItem label="Apply Function" />
-      </CtxSubMenu>
+      {children}
     </ControlledMenu>
   );
 };
