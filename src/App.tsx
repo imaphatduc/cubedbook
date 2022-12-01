9bd66af772940d0ecdc3ddd13dabb6beacdfa265
imaphatduc <vutranducphat@gmail.com>
2022-12-01 19:40:35 +0700

    Barebone context menu for group

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,20 @@
-import { MouseEvent, useState } from 'react';
-import { useMenuState } from '@szhsin/react-menu';
-
 import { PadSection } from './components/sections/PadSection';
 import { SceneSection } from './components/sections/SceneSection';
-import { CtxMenu } from './components/menu/CtxMenu';
-import { CtxSubMenu } from './components/menu/CtxSubMenu';
-import { CtxMenuItem } from './components/menu/CtxMenuItem';
 
 function App() {
-  const [menuProps, toggleMenu] = useMenuState();
-  const [anchorPoint, setAnchorPoint] = useState({ x: 0, y: 0 });
-
-  const openContextMenu = (e: MouseEvent) => {
-    e.preventDefault();
-    setAnchorPoint({ x: e.clientX, y: e.clientY });
-    toggleMenu(true);
-  };
-
   return (
     <div className="App">
-      <div
-        className="grid grid-cols-[1fr_2fr_1fr] bg-[#222] text-white"
-        onContextMenu={openContextMenu}
-      >
-        <div className="prose my-2 px-5">
+      <div className="grid grid-cols-[1fr_2fr_1fr] bg-[#222] text-white">
+        <div className="prose my-2 px-5 text-white">
           <SceneSection />
         </div>
         <div
           id="cubecubed"
           className="sticky top-0 right-0 bottom-0 left-0 bg-black h-screen"
         ></div>
-        <div className="prose my-2 px-5">
+        <div className="prose my-2 px-5 text-white">
           <PadSection />
         </div>
-
-        <CtxMenu
-          menuProps={menuProps}
-          toggleMenu={toggleMenu}
-          anchorPoint={anchorPoint}
-        >
-          <CtxSubMenu label="Geometry">
-            <CtxMenuItem label="Rectangle" />
-            <CtxMenuItem label="Square" />
-            <CtxMenuItem label="Circle" />
-            <CtxMenuItem label="Line" />
-            <CtxMenuItem label="Vector Shape" />
-            <CtxMenuItem label="Parametric Curve" />
-          </CtxSubMenu>
-          <CtxSubMenu label="Coordinates System">
-            <CtxMenuItem label="Axes" />
-          </CtxSubMenu>
-          <CtxSubMenu label="Animations">
-            <CtxMenuItem label="Create Shape" />
-            <CtxMenuItem label="Create Vector Shape" />
-            <CtxMenuItem label="Translate" />
-            <CtxMenuItem label="Rotate" />
-            <CtxMenuItem label="Fade In" />
-            <CtxMenuItem label="Fade Out" />
-            <CtxMenuItem label="Draw Grid" />
-            <CtxMenuItem label="Draw Axes" />
-            <CtxMenuItem label="Draw Vector Field" />
-            <CtxMenuItem label="Point To Coordinates" />
-            <CtxMenuItem label="Point Along Graph" />
-            <CtxMenuItem label="Write" />
-            <CtxMenuItem label="Trace" />
-            <CtxMenuItem label="Apply Function" />
-          </CtxSubMenu>
-        </CtxMenu>
       </div>
     </div>
   );
--- src/components/nodes/GroupNode.tsx
+++ src/components/nodes/GroupNode.tsx
@@ -1,9 +1,57 @@
+import { MenuButton } from '@szhsin/react-menu';
+import { Plus } from 'phosphor-react';
+
 import { IGroupNode } from '../../contexts/CubedContext';
+import { CtxMenuItem } from '../menu/CtxMenuItem';
+import { CtxSubMenu } from '../menu/CtxSubMenu';
+import { PressMenu } from '../menu/PressMenu';
 
 interface Props {
   groupNode: IGroupNode;
 }
 
 export const GroupNode = ({ groupNode }: Props) => {
-  return <div>{groupNode.group.name}</div>;
+  return (
+    <div>
+      <p className="m-0">{groupNode.group.name}</p>
+
+      <PressMenu
+        direction="right"
+        offsetY={-15}
+        menuButton={
+          <MenuButton className="hover:text-[#999] p-2 h-fit">
+            <Plus size={10} weight="bold" />
+          </MenuButton>
+        }
+      >
+        <CtxSubMenu label="Geometry">
+          <CtxMenuItem label="Rectangle" />
+          <CtxMenuItem label="Square" />
+          <CtxMenuItem label="Circle" />
+          <CtxMenuItem label="Line" />
+          <CtxMenuItem label="Vector Shape" />
+          <CtxMenuItem label="Parametric Curve" />
+        </CtxSubMenu>
+        <CtxSubMenu label="Coordinates System">
+          <CtxMenuItem label="Axes" />
+        </CtxSubMenu>
+        <CtxSubMenu label="Animations">
+          <CtxMenuItem label="Create Shape" />
+          <CtxMenuItem label="Create Vector Shape" />
+          <CtxMenuItem label="Translate" />
+          <CtxMenuItem label="Rotate" />
+          <CtxMenuItem label="Fade In" />
+          <CtxMenuItem label="Fade Out" />
+          <CtxMenuItem label="Draw Grid" />
+          <CtxMenuItem label="Draw Axes" />
+          <CtxMenuItem label="Draw Vector Field" />
+          <CtxMenuItem label="Point To Coordinates" />
+          <CtxMenuItem label="Point Along Graph" />
+          <CtxMenuItem label="Write" />
+          <CtxMenuItem label="Trace" />
+          <CtxMenuItem label="Apply Function" />
+        </CtxSubMenu>
+      </PressMenu>
+    </div>
+  );
 };
