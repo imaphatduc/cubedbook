5d68f14a380b605a4bbe8de3439b5c859e683f61
imaphatduc <vutranducphat@gmail.com>
2022-12-01 23:06:36 +0700

    Update `SquarePad`

---
+++ src/components/fields/InputField.tsx
@@ -0,0 +1,5 @@
+export const InputField = () => {
+  return (
+    <input className="max-w-[10rem] border-sm bg-[#0c0c0c] px-2 focus:outline-none" />
+  );
+};
--- src/components/pads/SquarePad.tsx
+++ src/components/pads/SquarePad.tsx
@@ -1,3 +1,33 @@
+import { InputField } from '../fields/InputField';
+
 export const SquarePad = () => {
-  return <div>SquarePad</div>;
+  return (
+    <div className="space-y-3">
+      <div className="flex justify-between">
+        <p className="m-0">name</p>
+        <InputField />
+      </div>
+      <div className="flex justify-between">
+        <p className="m-0">side_length</p>
+        <InputField />
+      </div>
+      <hr className="border-white opacity-20 pb-2" />
+      <div className="flex justify-between">
+        <p className="m-0">fill_color</p>
+        <InputField />
+      </div>
+      <div className="flex justify-between">
+        <p className="m-0">fill_opacity</p>
+        <InputField />
+      </div>
+      <div className="flex justify-between">
+        <p className="m-0">stroke_color</p>
+        <InputField />
+      </div>
+      <div className="flex justify-between">
+        <p className="m-0">stroke_width</p>
+        <InputField />
+      </div>
+    </div>
+  );
 };
