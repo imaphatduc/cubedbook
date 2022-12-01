9b9439493418f7712f6f56b1e663dfb4fdda6ccd
imaphatduc <vutranducphat@gmail.com>
2022-12-01 14:23:46 +0700

    Tweak app sections

--- src/components/sections/NodesSection.tsx
+++
@@ -1,9 +0,0 @@
-import SectionHeader from './components/SectionHeader';
-
-export const NodesSection = () => {
-  return (
-    <div>
-      <SectionHeader header="Typewriter" />
-    </div>
-  );
-};
--- src/components/sections/SceneSection.tsx
+++ src/components/sections/SceneSection.tsx
@@ -3,7 +3,7 @@ import SectionHeader from './components/SectionHeader';
 export const SceneSection = () => {
   return (
     <div>
-      <SectionHeader header="Scene" />
+      <SectionHeader header="Manuscript" />
     </div>
   );
 };
