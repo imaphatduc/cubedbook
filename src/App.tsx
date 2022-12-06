8b779b223d2bcf4a7a08131ba0f0662d27f76ddc
imaphatduc <vutranducphat@gmail.com>
2022-12-06 21:01:41 +0700

    Animation section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { AnimationSection } from './components/sections/AnimationSection';
 import { PadSection } from './components/sections/PadSection';
 import { SceneSection } from './components/sections/SceneSection';
 
@@ -7,18 +8,27 @@ function App() {
 
   return (
     <div className="App">
-      <div className="grid grid-cols-[1fr_2fr_1fr] bg-[#222] text-white">
-        <div className="prose my-2 px-5 text-white">
-          <SceneSection
-            currentNodeId={currentNodeId}
-            setCurrentNodeId={setCurrentNodeId}
-          />
+      <div className="grid grid-cols-[3fr_1fr] bg-[#222] text-white">
+        <div className="grid grid-rows-[2fr_1fr]">
+          <div className="grid grid-cols-[1fr_2fr]">
+            <div className="prose my-2 px-5 text-white border-r-2 border-r-gray-500">
+              <SceneSection
+                currentNodeId={currentNodeId}
+                setCurrentNodeId={setCurrentNodeId}
+              />
+            </div>
+            <div
+              id="cubecubed"
+              className="sticky top-0 right-0 bottom-0 left-0 bg-black h-[66.6vh]"
+            ></div>
+          </div>
+
+          <div className="row-span-2 border-t-2 border-t-gray-500 bg-[#111]">
+            <AnimationSection />
+          </div>
         </div>
-        <div
-          id="cubecubed"
-          className="sticky top-0 right-0 bottom-0 left-0 bg-black h-screen"
-        ></div>
-        <div className="prose my-2 px-5 text-white">
+
+        <div className="prose my-2 px-5 text-white border-l-2 border-l-gray-500">
           <PadSection currentNodeId={currentNodeId} />
         </div>
       </div>
---
+++ src/components/sections/AnimationSection.tsx
@@ -0,0 +1,3 @@
+export const AnimationSection = () => {
+  return <div>AnimationSection</div>;
+};
