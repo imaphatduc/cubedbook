d1123ef4e6a54b238d634d9c8712435365ae2a55
imaphatduc <vutranducphat@gmail.com>
2025-12-03 14:02:12 +0700

    working animation

--- src/features/animation/components/PlayButton.tsx
+++ src/features/animation/components/PlayButton.tsx
@@ -1,8 +1,34 @@
+import { useCubed } from "@/contexts";
 import { Play } from "phosphor-react";
 
 export const PlayButton = () => {
+  const { scene, groupNodes } = useCubed();
+
+  const play = () => {
+    const animationQueues = groupNodes
+      .map((groupNode) => groupNode.animationQueueNodes)
+      .flat()
+      .sort((a, b) => a.startTime - b.startTime);
+
+    animationQueues.forEach((queue) => {
+      console.log(queue);
+
+      scene.play(
+        queue.animationNodes
+          .map((node) => {
+            if (node.animation) {
+              node.animation.sleepTime = queue.startTime;
+            }
+
+            return node.animation;
+          })
+          .filter((animation) => !!animation)
+      );
+    });
+  };
+
   return (
-    <button>
+    <button onClick={play}>
       <Play size={20} weight="fill" />
     </button>
   );
